b20cd94ef9879229139649dcac0a6636bd05df14
vineeshw <120644914+vineeshw@users.noreply.github.com>
2023-06-12 23:34:21 +0530

    Update A6.js
    adding the prime number finding solution

--- A6.js
+++ A6.js
@@ -11,3 +11,22 @@ for (let i = 0; i < array.length; i++) {
 }
 
 console.log(uniqueArray); // Output: [1, 2, 3, 4, 5, 6]
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// finding the prime numbers  in an array
+
+const array = [1,2,3,4,5,6,7,8,9]
+var flag
+
+for(let i=0; i<array.length; i++){
+    flag = 0
+    for(let j=2; j<array[i]; j++){
+        if(array[i] % j === 0){
+            flag = 1;
+            break;
+        }
+    }
+    if(flag === 0){
+        console.log(array[i]);
+    }
+}
